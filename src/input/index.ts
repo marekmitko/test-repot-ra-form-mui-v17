30e95513188f1b8ad78cf93a23da526a1bffc141
marekmitko <63197096+marekmitko@users.noreply.github.com>
2021-07-27 10:56:53 +0200

    init repo

---
+++ src/input/index.ts
@@ -0,0 +1,91 @@
+import ArrayInput, { ArrayInputProps } from './ArrayInput';
+import AutocompleteArrayInput, {
+    AutocompleteArrayInputProps,
+} from './AutocompleteArrayInput';
+import AutocompleteInput, { AutocompleteInputProps } from './AutocompleteInput';
+import BooleanInput from './BooleanInput';
+import CheckboxGroupInput, {
+    CheckboxGroupInputProps,
+} from './CheckboxGroupInput';
+import DateInput, { DateInputProps } from './DateInput';
+import DateTimeInput, { DateTimeInputProps } from './DateTimeInput';
+import FileInput, { FileInputProps } from './FileInput';
+import ImageInput, { ImageInputProps } from './ImageInput';
+import InputHelperText, { InputHelperTextProps } from './InputHelperText';
+import InputPropTypes from './InputPropTypes';
+import Labeled, { LabeledProps } from './Labeled';
+import NullableBooleanInput, {
+    NullableBooleanInputProps,
+} from './NullableBooleanInput';
+import NumberInput, { NumberInputProps } from './NumberInput';
+import PasswordInput, { PasswordInputProps } from './PasswordInput';
+import RadioButtonGroupInput, {
+    RadioButtonGroupInputProps,
+} from './RadioButtonGroupInput';
+import ReferenceArrayInput, {
+    ReferenceArrayInputProps,
+} from './ReferenceArrayInput';
+import ReferenceInput, { ReferenceInputProps } from './ReferenceInput';
+import ResettableTextField, {
+    ResettableTextFieldProps,
+} from './ResettableTextField';
+import SearchInput, { SearchInputProps } from './SearchInput';
+import SelectArrayInput, { SelectArrayInputProps } from './SelectArrayInput';
+import SelectInput, { SelectInputProps } from './SelectInput';
+import TextInput, { TextInputProps } from './TextInput';
+import sanitizeInputRestProps from './sanitizeInputRestProps';
+export * from './TranslatableInputs';
+export * from './TranslatableInputsTabContent';
+export * from './TranslatableInputsTabs';
+export * from './TranslatableInputsTab';
+
+export {
+    ArrayInput,
+    AutocompleteArrayInput,
+    AutocompleteInput,
+    BooleanInput,
+    CheckboxGroupInput,
+    DateInput,
+    DateTimeInput,
+    FileInput,
+    ImageInput,
+    InputHelperText,
+    InputPropTypes,
+    Labeled,
+    NullableBooleanInput,
+    NumberInput,
+    PasswordInput,
+    RadioButtonGroupInput,
+    ReferenceArrayInput,
+    ReferenceInput,
+    ResettableTextField,
+    SearchInput,
+    SelectArrayInput,
+    SelectInput,
+    TextInput,
+    sanitizeInputRestProps,
+};
+
+export type {
+    ArrayInputProps,
+    AutocompleteInputProps,
+    AutocompleteArrayInputProps,
+    CheckboxGroupInputProps,
+    DateInputProps,
+    DateTimeInputProps,
+    FileInputProps,
+    ImageInputProps,
+    InputHelperTextProps,
+    LabeledProps,
+    NullableBooleanInputProps,
+    NumberInputProps,
+    PasswordInputProps,
+    RadioButtonGroupInputProps,
+    ReferenceArrayInputProps,
+    ReferenceInputProps,
+    ResettableTextFieldProps,
+    SearchInputProps,
+    SelectArrayInputProps,
+    SelectInputProps,
+    TextInputProps,
+};
